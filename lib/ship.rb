9eb63336ce78239e4868c81b4d5d7f643d8b7bfd
sanjsanj <sanjsanj@hotmail.com>
2015-03-24 10:11:07 +0000

    Add ship unit test to detect hit

---
+++ lib/ship.rb
@@ -0,0 +1,8 @@
+# ship
+class Ship
+  attr_reader :length
+
+  def initialize(length = 1)
+    @length = length
+  end
+end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -4,4 +4,9 @@ describe Ship do
   it 'can be initiated with a length' do
     expect(subject.length).not_to eq 0
   end
+
+  it 'can be hit' do
+    subject.hit
+    expect(subject.hit_count).to eq 1
+  end
 end
