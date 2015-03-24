50a1fd6ad95015c6645e415f5f898fa293ec3059
sanjsanj <sanjsanj@hotmail.com>
2015-03-24 13:36:07 +0000

    Add mvp feature tests for 'can hit' and 'can sink ship'

--- spec/feature/mvp_spec.rb
+++ spec/feature/mvp_spec.rb
@@ -1,8 +1,26 @@
 require 'capybara/rspec'
+require 'ship'
+require 'board'
 
 feature 'MVP feature test' do
+  let(:board) { Board.new }
+  let(:ship) { Ship.new }
   scenario 'ship can be placed on board' do
-    board.place ship
-    expect(board.cell 'A1').to eq ship
+    board.place_ship ship
+    expect(board.grid['A1']).to eq ship
+  end
+
+  scenario 'can hit ship' do
+    board.place_ship ship
+    board.place_shot('A1')
+    expect(board.hit_grid['A1']).to eq 'Red'
+  end
+
+  scenario 'can sink ship' do
+    board.place_ship ship
+    board.place_shot('A1')
+    board.place_shot('A2')
+    expect(board.hit_grid['A1']).to eq 'SunkShip'
+    expect(board.hit_grid['A2']).to eq 'SunkShip'
   end
 end
